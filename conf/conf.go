761101fffee7201a17be62b810e60a471c6002dc
liujinbao <liujinbao@mafengwo.com>
2019-09-03 22:32:17 +0800

    [master]

---
+++ conf/conf.go
@@ -0,0 +1,10 @@
+package conf
+
+import(
+	"github.com/spf13/viper"
+)
+
+//Setup for example
+func Setup()  {
+	viper.SetConfigType("YAML")
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module market_monitor
+
+go 1.12
+
+require github.com/spf13/viper v1.4.0
